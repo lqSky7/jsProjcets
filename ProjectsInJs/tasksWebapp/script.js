992d545d4339f57d8cdc5b8203a24bed4aed2e20
lqsky7 <catinice@outlook.com>
2024-10-23 21:31:59 +0530

    render tasks

--- ProjectsInJs/tasksWebapp/script.js
+++ ProjectsInJs/tasksWebapp/script.js
@@ -10,31 +10,39 @@ TaskList.forEach(element => {
 });
 
 
+
 // grab text when todoInput is pressed.
 taskButton.addEventListener("click", () => {
     textGivenByUser = todoInput.value.trim()
-    if(textGivenByUser === "") {return;}
+    if(textGivenByUser === "") return;
 
     const DataToPush_toTasklist = {
         UID: Date.now(),
         content: textGivenByUser,
         TaskCompletedStatus: false,
-    }
-    
-
-    // function to push data to local storage.
-    function pushData_toLocal() {
-        localStorage.setItem("keyforpushData_toLocal", JSON.stringify(TaskList));
-    }
+    };
 
     TaskList.push(DataToPush_toTasklist)    
     todoInput.value = ""
     pushData_toLocal()
+    rendertask(DataToPush_toTasklist)
     console.log(TaskList)
 })
 
-function rendertask(list){
-    console.log(list)
+function rendertask(datatorender){
+    const li = document.createElement('li');
+    //basically setting up some identification for this const.
+    li.setAttribute("list-UID", datatorender.UID)
+    li.innerHTML = `<span>${datatorender.content}</span>
+    <button>Delete</button>`
+    todoList.append(li)
+
+    
+}
+
+    // function to push data to local storage.
+function pushData_toLocal() {
+    localStorage.setItem("keyforpushData_toLocal", JSON.stringify(TaskList));
 }
 
 
